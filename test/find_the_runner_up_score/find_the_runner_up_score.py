d44174a582f99a9765ef1a7618826d0f70a1d2fb
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-12 18:55:42 +0530

    this is testcase for runner up

---
+++ test/find_the_runner_up_score/find_the_runner_up_score.py
@@ -0,0 +1,36 @@
+import unittest
+
+from src.find_the_runner_up_score.util import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test1(self):
+        actual_input = find_runner_up()
+        expected_output = 5
+        self.assertEqual(actual_input, expected_output)  # add assertion here
+        '''
+        5
+2 3 6 6 5
+        '''
+
+    def test2(self):
+        actual_input = find_runner_up()
+        expected_output = 6
+        self.assertEqual(actual_input, expected_output)  # add assertion here
+        '''
+        6
+5 6 4 7 5 3
+        '''
+
+    def test3(self):
+        actual_input = find_runner_up()
+        expected_output = 7
+        self.assertEqual(actual_input, expected_output)  # add assertion here
+        '''
+        7
+4 6 5 7 4 9 6
+        '''
+
+
+if __name__ == '__main__':
+    unittest.main()
