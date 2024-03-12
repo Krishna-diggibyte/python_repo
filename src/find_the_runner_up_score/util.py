eb536d4619b3e7473a73b7b4368bd60fbe9f9e07
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-12 19:02:05 +0530

    updated  util file of runner up score problem

--- src/find_the_runner_up_score/util.py
+++ src/find_the_runner_up_score/util.py
@@ -1,4 +1,11 @@
-def find_runner_up(new_list):
+import logging
+
+logging.basicConfig(level=logging.INFO ,format='%(message)s')
+def find_runner_up():
+    n = int(input())
+    arr = map(int, input().split())
+    new_list = (list(arr))
+    new_list.sort()
     mmax = new_list[1]
     bmax = new_list[0]
 
@@ -6,5 +13,7 @@ def find_runner_up(new_list):
         if (i > mmax):
             bmax = mmax
             mmax = i;
-    print(bmax)
+    logging.info(bmax)
+    return bmax
+
 
