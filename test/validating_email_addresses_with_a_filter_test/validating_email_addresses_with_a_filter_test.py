4ab39cb58e8675a84e213b97053009478c649643
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-14 18:01:09 +0530

    update test cases of validating email addresses

--- test/validating_email_addresses_with_a_filter_test/validating_email_addresses_with_a_filter_test.py
+++ test/validating_email_addresses_with_a_filter_test/validating_email_addresses_with_a_filter_test.py
@@ -16,26 +16,25 @@ britts_54@hackerrank.com
 
     def test2(self):
         actual_input = validating_email()
-        expected_output = 'SATURDAY'
+        expected_output = ['Amit@diggibyte.com', 'Basheer@diggibyte.com', 'Krishna@diggibyte.com']
         self.assertEqual(actual_input, expected_output)
         '''
         sample input 
 3
-lara@hackerrank.com
-brian-23@hackerrank.com
-britts_54@hackerrank.com
+Krishna@diggibyte.com
+Basheer@diggibyte.com
+Amit@diggibyte.com
         '''
 
     def test3(self):
         actual_input = validating_email()
-        expected_output = 'MONDAY'
+        expected_output = ['kalai@diggibyte.com', 'prathibha@diggibyte.com']
         self.assertEqual(actual_input, expected_output)
         '''
         sample input 
-3
-lara@hackerrank.com
-brian-23@hackerrank.com
-britts_54@hackerrank.com
+2
+kalai@diggibyte.com
+prathibha@diggibyte.com
         '''
 
 if __name__ == '__main__':
