c3dea7eeb44283fd3de2f8a6041e863e5abb4f95
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-14 17:23:52 +0530

    testcases added for find the runner up score

--- test/find_the_runner_up_score_test/find_the_runner_up_score_test.py
+++ test/find_the_runner_up_score_test/find_the_runner_up_score_test.py
@@ -1,13 +1,11 @@
 import unittest
-
 from src.find_the_runner_up_score.util import *
-
 
 class MyTestCase(unittest.TestCase):
     def test1(self):
         actual_input = find_runner_up()
         expected_output = 5
-        self.assertEqual(actual_input, expected_output)  # add assertion here
+        self.assertEqual(actual_input, expected_output)
         '''
         5
 2 3 6 6 5
@@ -16,8 +14,9 @@ class MyTestCase(unittest.TestCase):
     def test2(self):
         actual_input = find_runner_up()
         expected_output = 6
-        self.assertEqual(actual_input, expected_output)  # add assertion here
+        self.assertEqual(actual_input, expected_output)
         '''
+        sample input 
         6
 5 6 4 7 5 3
         '''
@@ -25,12 +24,11 @@ class MyTestCase(unittest.TestCase):
     def test3(self):
         actual_input = find_runner_up()
         expected_output = 7
-        self.assertEqual(actual_input, expected_output)  # add assertion here
+        self.assertEqual(actual_input, expected_output)
         '''
         7
 4 6 5 7 4 9 6
         '''
-
 
 if __name__ == '__main__':
     unittest.main()
