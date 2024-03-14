7ead1b42e80fed5495fc21104dfebd062bdef1bf
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-03-14 17:32:44 +0530

    driver file for mutations

--- src/Mutations/driver.py
+++ src/Mutations/driver.py
@@ -1,12 +1,7 @@
-def mutate_string(string, position, character):
-    l = list(string)
-    l[position] = character
-    string = ''.join(l)
-    return string
+
+from src.mutations.util import mutate_string
+
+mutate_string()
 
 
-if __name__ == '__main__':
-    s = input()
-    i, c = input().split()
-    s_new = mutate_string(s, int(i), c)
-    print(s_new)
+
